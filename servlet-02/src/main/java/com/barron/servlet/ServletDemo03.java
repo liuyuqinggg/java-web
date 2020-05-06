d050b2680f1f7ce122f4fd9462ec6253cc9fa804
liuyuqinggg <lyq1464972196@gmail.com>
2020-05-06 18:01:09 +0800

    .

---
+++ servlet-02/src/main/java/com/barron/servlet/ServletDemo03.java
@@ -0,0 +1,25 @@
+package com.barron.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo03 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext context = this.getServletContext();
+        String url = context.getInitParameter("url");
+
+        System.out.println(url);
+        resp.getWriter().print(url);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
